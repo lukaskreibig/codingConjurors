d3c706ec1a2bfa00c3d030edb61a141c35a65316
lukaskreibig <lukatze@posteo.de>
2021-03-20 19:51:32 +0100

    add animate dots on progress bar

--- script.js
+++ script.js
@@ -2,43 +2,56 @@ let innerWrapper = document.querySelector('.inner-wrapper');
 let outerWrapper = document.querySelector('.outer-wrapper')
 
 //onscroll start function
-outerWrapper.onscroll = () => { progressBar(); };
+outerWrapper.onscroll = () => { progressBar(); progressStep()};
 
-progressBar = () => {
+      progressBar = () => {
 
-  // position of Users scrolling
-  let winScroll = outerWrapper.scrollTop;
-  // complete scroll width of inner Wrapper - clientHeight from outer Wrapper = scrolled to 100%
-  let width = innerWrapper.scrollWidth - outerWrapper.clientHeight;
-  // position of user / width * 100 = percentage where user is
-  let scrolled = (winScroll / width) * 100;
-  // width of progress bar equals width of percentage where user is
-  document.getElementById("progress-bar").style.width = scrolled + "%";
-}
-
-
-//onscroll start function
-// outerWrapper.onscroll = () => { progressStep(); };
+        // position of Users scrolling
+        let winScroll = outerWrapper.scrollTop;
+        // complete scroll width of inner Wrapper - clientHeight from outer Wrapper = scrolled to 100%
+        let width = innerWrapper.scrollWidth - outerWrapper.clientHeight;
+        // position of user / width * 100 = percentage where user is
+        let scrolled = (winScroll / width) * 100;
+        // width of progress bar equals width of percentage where user is
+        document.getElementById("progress-bar").style.width = scrolled + "%";
+      }
 
 
-// progressStep = () => {
-//   let progressStep = (document.getElementById("progress-bar"))
 
-//  switch(progressStep.style.width) {
-//     case "0%":
-    
-//     break;
-//     case "25%":
-    
-//     break;
-//     case "50%":
-    
-//     break;
-//     case "75%":
-    
-//     break;
-//     case "100%":
-    
-//     break;
-// }
-// }
+      progressStep = () => {
+        let progressDiv = (document.getElementById("progress-bar"))
+        let dot2 = document.getElementById("dot2")
+        let dot3 = document.getElementById("dot3")
+        let dot4 = document.getElementById("dot4")
+        let dot5 = document.getElementById("dot5")
+
+         
+        let progressString = progressDiv.style.width
+        let progress = Number.parseInt(progressString) 
+        console.log(typeof(progress))
+        console.log(progress) 
+                  
+                  if(progress > -1 && progress <= 25) 
+                  {
+                  dot2.style.display = "none";
+                  }
+                  else if(progress > 25 && progress <= 50)
+                  {
+                  dot2.style.display = "block";
+                  dot3.style.display = "none";
+                  }
+                  else if(progress > 50 && progress <= 75)
+                  {
+                  dot3.style.display = "block";
+                  dot4.style.display = "none";
+                  }
+                  else if (progress > 75 && progress <= 99)
+                  {
+                  dot4.style.display = "block";
+                  dot5.style.display = "none";
+                  }
+                  else if (progress == 100)
+                  {
+                  dot5.style.display = "block";
+                  }                 
+      }
