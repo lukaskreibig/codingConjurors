e0548addd5a88c829c8134109a84c9b88dfea021
lukaskreibig <lukatze@posteo.de>
2021-03-20 21:01:36 +0100

    add leveltext other small updates

--- script.js
+++ script.js
@@ -21,14 +21,20 @@ outerWrapper.onscroll = () => { progressBar(); progressStep()};
       // this function handles the appearance of the progress dots
       progressStep = () => {
 
-        // get all the CSS
-        let progressDiv = (document.getElementById("progress-bar"))
+        // get all the CSS for the dots
         let dot2 = document.getElementById("dot2")
         let dot3 = document.getElementById("dot3")
         let dot4 = document.getElementById("dot4")
         let dot5 = document.getElementById("dot5")
 
+        // get all the CSS for the text
+        let text2 = document.getElementById("progress-text2")
+        let text3 = document.getElementById("progress-text3")
+        let text4 = document.getElementById("progress-text4")
+        let text5 = document.getElementById("progress-text5")
+
         // turn the scroll percentage from string to number
+        let progressDiv = (document.getElementById("progress-bar"))
         let progressString = progressDiv.style.width
         let progress = Number.parseInt(progressString) 
         // console.log(typeof(progress))
@@ -39,24 +45,37 @@ outerWrapper.onscroll = () => { progressBar(); progressStep()};
                   if(progress > -1 && progress <= 11) 
                   {
                   dot2.style.display = "none";
+
+                  text2.style.display = "none";
                   }
                   else if(progress > 11 && progress <= 35)
                   {
                   dot2.style.display = "block";
                   dot3.style.display = "none";
+
+                  text2.style.display = "block";
+                  text3.style.display = "none";
                   }
                   else if(progress > 35 && progress <= 61)
                   {
                   dot3.style.display = "block";
                   dot4.style.display = "none";
+
+                  text3.style.display = "block";
+                  text4.style.display = "none";
                   }
                   else if (progress > 61 && progress <= 87)
                   {
                   dot4.style.display = "block";
                   dot5.style.display = "none";
+
+                  text4.style.display = "block";
+                  text5.style.display = "none";
                   }
                   else if (progress > 88)
                   {
                   dot5.style.display = "block";
+
+                  text5.style.display = "block";
                   }                 
       }
