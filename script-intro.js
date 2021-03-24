3c1db4ce535b7a4ea8f3ecf486a9af34827dcabd
lukaskreibig <lukatze@posteo.de>
2021-03-24 14:02:16 +0100

    riddle update

--- script-intro.js
+++ script-intro.js
@@ -1,10 +1,12 @@
 let i = 0;
 let speed = 50;
-let intro = '....the statue starts to speak as you approach the old ruins....';
+let statue1 = '....the statue starts to speak as you approach the old ruins....';
+let statue2 = 'BEHOLD! YOU DARE to enter the forgotten magical world of coding? Are YOU worthy to do so? Answer my question, and we\'ll see if you are as clever as you think. Who was the great inventor of the magical coding spell language, also known as JAVASCRIPT, which brought magic upon the Internet?'
 
-function typeWriter(txt) {
-  if (i < txt.length) {
-    document.getElementById("riddlebox").innerHTML += txt.charAt(i);
+const typeWriter = () => {
+    console.log(statue1.length)
+  if (i < statue1.length) {
+    document.getElementById("riddlebox2").innerHTML += statue1.charAt(i)
     i++;
     setTimeout(typeWriter, speed);
   }
