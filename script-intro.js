2741002214dfc45a477641e3d327aa2757402511
lukaskreibig <lukatze@posteo.de>
2021-03-24 12:14:03 +0100

    try out typewriter jscript

--- script-intro.js
+++ script-intro.js
@@ -1,13 +1,13 @@
 let i = 0;
-let intro = '....the statue starts to speak as you approach the old ruins....';
 let speed = 50;
+let intro = '....the statue starts to speak as you approach the old ruins....';
 
-function typeWriter(text) {
-  if (i < text.length) {
-    document.getElementById("riddlebox").innerHTML += text.charAt(i);
+function typeWriter(txt) {
+  if (i < txt.length) {
+    document.getElementById("riddlebox").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }
 
-typeWriter(intro)
+typeWriter()
