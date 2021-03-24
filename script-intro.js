a12a7a6a98718e25b60a31bb7bbf2bbcb5cbb5ed
lukaskreibig <lukatze@posteo.de>
2021-03-24 16:38:39 +0100

    buttons disappear after riddle solve

--- script-intro.js
+++ script-intro.js
@@ -1,17 +1,17 @@
 let i = 0;
-let spoken = 0
 let buttonswitch = 0
 let speed = 15;
+let button1 = document.getElementById("b1")
+let button2 = document.getElementById("b2")
+let button3 = document.getElementById("b3")
 let statue1 = '....the statue starts to speak as you approach the old ruins....';
-let statue2 = 'BEHOLD! YOU DARE to enter the forgotten magical world of coding? Are YOU worthy to do so? Answer my question, and we\'ll see if you are as clever as you think. Who was the great inventor of the magical coding spell language, also known as JAVASCRIPT, which brought magic upon the Internet? Choose wisely...'
+let statue2 = "BEHOLD! YOU DARE to enter the forgotten magical world of coding? Are YOU worthy to do so? Answer my question, and we\'ll see if you are as clever as you think. Who was the great inventor of the magical coding spell language, also known as JAVASCRIPT, which brought magic upon the Internet? Choose wisely..."
 
 const typeWriter = () => {
   if (i < statue2.length) {
     document.getElementById("riddlebox2").innerHTML += statue2.charAt(i)
     i++;
     setTimeout(typeWriter, speed)
-    return spoken = 1
-
   }
 }
 
@@ -34,7 +34,16 @@ document.getElementById("b1").addEventListener("click", function() {
             button.innerHTML = "<a href=index.html>CODING IS MAGIC</a></span>"
             button.classList.add("entry");
             document.getElementById("riddlebox").appendChild(button);
-            buttonswitch = 1        
+            buttonswitch = 1 
+            button1.style.display = "none";
+            button2.style.display = "none";
+            button3.style.display = "none";
+            
+            
+
+dot2.style.display = "block";
+dot3.style.display = "none";
+
         }
         else{null}
   }); 
